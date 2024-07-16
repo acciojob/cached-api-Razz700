7ac3474aa12a0ca75b578758876767aa3a8f7dc4
Razzakh <mrazzakh123@gmail.com>
2024-07-16 13:45:31 +0530

    first

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,42 @@
 
-import React from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import './../styles/App.css';
+import axios from "axios";
+
 
 const App = () => {
+  const [input,setinput]=useState('https://jsonplaceholder.typicode.com/posts');
+  const [data,setdata]=useState([]);
+  const data1=useMemo(()=>{
+    storedata();
+        },[input]);
+     function storedata(){
+     console.log('in use memo');
+     return data;
+    }
+  useEffect(()=>{
+    axios.request(input).then((res)=>{
+      console.log(res.data);
+      setdata(res.data);
+    });
+},[]);
+
+  
+ 
   return (
     <div>
+    eeeeee
+      <ul>
+      {
+  data.map((item,i)=> 
+  <li key={i+'a'}>
+  <h3>{item.title}</h3>
+  <p>{item.body}</p>
+</li>
+  )
+}
+      </ul>
+
         {/* Do not remove the main div */}
     </div>
   )
