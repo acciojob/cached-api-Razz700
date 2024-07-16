f1152baa9c83b0929730f767719b936e4f887272
Razzakh <mrazzakh123@gmail.com>
2024-07-16 14:18:16 +0530

    next

--- src/components/App.js
+++ src/components/App.js
@@ -5,8 +5,10 @@ import axios from "axios";
 
 
 const App = () => {
+  const [loading,setloading]=useState(true);
   const [input,setinput]=useState('https://jsonplaceholder.typicode.com/posts');
   const [data,setdata]=useState([]);
+  const [c,setc]=useState(0);
   const data1=useMemo(()=>{
     storedata();
         },[input]);
@@ -14,18 +16,20 @@ const App = () => {
      console.log('in use memo');
      return data;
     }
+ 
   useEffect(()=>{
     axios.request(input).then((res)=>{
       console.log(res.data);
       setdata(res.data);
+      setloading(false);
     });
-},[]);
+},[c]);
 
   
  
   return (
     <div>
-    eeeeee
+      {loading && <div>Loading...</div> }
       <ul>
       {
   data.map((item,i)=> 
